8d413edbb6eea9334d1ad9d5effc3c5e5e5ea488
112B12711 <145737033+112B12711@users.noreply.github.com>
2024-04-11 19:37:13 +0800

    Add files via upload

---
+++ ch5/C05_01.sql
@@ -0,0 +1,9 @@
+CREATE VIEW ShohinSum (shohin_catalg, cnt_shohin)
+AS
+SELECT shohin_catalg, COUNT(*)
+  FROM Shohin
+ GROUP BY shohin_catalg;
+
+
+SELECT shohin_catalg, cnt_shohin
+  FROM ShohinSum;
